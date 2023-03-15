072ec655c5491d73aa0535b2bee03136b7e50c2b
vidyadharek <125987647+vidyadharek@users.noreply.github.com>
2023-03-15 22:30:40 +0530

    Update App.java

--- src/main/java/com/selenium/test_my_adderessbook/App.java
+++ src/main/java/com/selenium/test_my_adderessbook/App.java
@@ -21,7 +21,7 @@ public class App
     	   
     	    //object of chrome options
     	    ChromeOptions chromeOptions = new ChromeOptions();
-            //chromeOptions.addArguments("--headless");
+            chromeOptions.addArguments("--headless");
     	    chromeOptions.addArguments("--remote-allow-origins=*");
             chromeOptions.addArguments("--disable-dev-shm-usage");
             chromeOptions.addArguments("--disable-extensions");
