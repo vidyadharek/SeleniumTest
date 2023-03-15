ce4e40d4c3bd3715cca12cd6da196927f15a22c1
vidyadharek <125987647+vidyadharek@users.noreply.github.com>
2023-03-15 22:25:14 +0530

    Update App.java

--- src/main/java/com/selenium/test_my_adderessbook/App.java
+++ src/main/java/com/selenium/test_my_adderessbook/App.java
@@ -24,9 +24,9 @@ public class App
             //chromeOptions.addArguments("--headless");
     	    chromeOptions.addArguments("--remote-allow-origins=*");
             chromeOptions.addArguments("--disable-dev-shm-usage");
-            chrome_options.add_argument("--disable-extensions");
-            chrome_options.add_argument("--disable-gpu");
-            chrome_options.add_argument("--no-sandbox");
+            chrome_options.addArguments("--disable-extensions");
+            chrome_options.addArguments("--disable-gpu");
+            chrome_options.addArguments("--no-sandbox");
            // driver = webdriver.Chrome(options=chrome_options);
         
        // chrome_options = Options()
